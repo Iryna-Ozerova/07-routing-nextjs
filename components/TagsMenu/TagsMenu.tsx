785082f85438e40b99acb3f61902d6181c8ec9a4
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-07-09 01:05:24 +0300

    edit TagsMenu

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -1,23 +1,30 @@
+'use client'
+
+import { useState } from "react";
 import css from "./TagsMenu.module.css"
 import Link from "next/link"
 
 const tags = ["All", "Work", "Personal", "Meeting", "Shopping", "Todo"]
 
 const TagsMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
     return (
         <div className={css.menuContainer}>
-  <button className={css.menuButton}>
+  <button onClick={toggle} className={css.menuButton}>
     Notes ▾
   </button>
-            <ul className={css.menuList}>
-                {tags.map((tag) => (
-                    <li key={tag} className={css.menuItem}>
-                        <Link href={`/notes/filter/${tag === "All" ? "all" : tag}`} className={css.menuLink}>
-  {tag}
-</Link>
-                    </li>
-                ))}
-    </ul>
+            {isOpen && (
+                <ul className={css.menuList}>
+                    {tags.map((tag) => (
+                        <li key={tag} className={css.menuItem}>
+                            <Link onClick={toggle} href={`/notes/filter/${tag === "All" ? "all" : tag}`} className={css.menuLink}>
+                                {tag}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
 </div>
     )
 }
